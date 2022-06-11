61972f898dfd46f9c624c31006664e89b1ba38a3
Oscar Velandia <oscar.daniel.v.s.235@gmail.com>
2022-06-11 16:56:01 -0500

    Add env variables with react-native-dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: ['YELP_API_KEY', 'YELP_CLIENT_ID'],
+      },
+    ],
+  ],
 };
---
+++ types/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const YELP_API_KEY: string;
+  export const YELP_CLIENT_ID: string;
+}
